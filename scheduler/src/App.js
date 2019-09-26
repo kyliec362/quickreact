72075f5992bf6239f55a3780b80c3bd195684a0a
Kylie Chesner <kyliec362@gmail.com>
2019-09-26 12:47:12 -0500

    Slice 1: Our first React code

---
+++ scheduler/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
+
+const App = () =>  (
+  <div>
+    <h1>{ schedule.title }</h1>
+  </div>
+);
+
+export default App;
